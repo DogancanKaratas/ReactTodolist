629ef545458a6e15092bca6c0dd9b669aad2462f
DogancanKaratas <karatasdgncn21@gmail.com>
2023-12-11 17:17:35 +0300

    Add filter functionality to Todo list

--- src/components/TodoLİst.jsx
+++ src/components/TodoLİst.jsx
@@ -6,7 +6,13 @@ const TodoLİst = ({ todos, setTodos }) => {
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo text={todo.text} key={todo.id} />
+          <Todo
+            text={todo.text}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+            todo={todo}
+          />
         ))}
       </ul>
     </div>
